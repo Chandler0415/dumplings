9a7d095794897db393a722ab40ae7e6ba2d09bb0
hejingguo <hejingguo@meituan.com>
2016-12-16 17:56:32 +0800

    字符串插入dom

---
+++ src/addNodeChip.js
@@ -0,0 +1,19 @@
+/**
+ * 在指定位置做dom片段插入
+ * @param {string} headStr dom片段
+ * @param {string} containerId 容器Id
+ */
+var addNodeChip = function(domStr, containerId) {
+  var snippet = document.createTextNode(domStr);
+  var body = document.getElementsByTagName('body')[0];
+
+  //如果没有容器的话，则在body的最上方插入
+  if (typeof containerId === 'undefined') {
+    body.insertBefore(snippet, body.childNodes[0]);
+  } else {
+    var container = document.getElementById(containerId);
+    container.innerHTML = snippet;
+  }
+}
+
+window.addNodeChip = addNodeChip;
