f8267225021d867bea8d3c73473dd20cb8bf63e7
hejingguo <hejingguo@meituan.com>
2017-02-08 17:51:47 +0800

    xxx

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-var tpl = '<div>' +
-  '<h1>这里是公用的头</h1>' +
-  '</div>';
+/**
+ * 在指定位置做dom片段插入
+ * @param {string} headStr dom片段
+ * @param {string} containerId 容器Id
+ */
+var addDom = function(domStr, containerId) {
+  var snippet = document.createTextNode(domStr);
+  var body = document.getElementsByTagName('body')[0];
 
+  //如果没有容器的话，则在body的最上方插入
+  if (typeof containerId === 'undefined') {
+    body.insertBefore(snippet, body.childNodes[0]);
+  } else {
+    var container = document.getElementById(containerId);
+    container.innerHTML = snippet;
+  }
+}
 
-var body = document.getElementsByTagName('body')[0];
-
-//insert到body的最前面
-//
+window.addDom = addDom;
